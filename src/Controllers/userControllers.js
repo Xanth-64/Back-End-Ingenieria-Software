1316f7b678852b6e70af61f9512e9fd7b5058639
Andres Eloy <30258079@correo.unimet.edu.ve>
2021-05-16 11:36:29 -0400

    Configuracion Basica de la DB con MAS Comentarios

---
+++ src/Controllers/userControllers.js
@@ -0,0 +1,9 @@
+//TODO Definir Controladores para las rutas Basicas de los Usuarios
+
+//GET de 1 Usuario
+
+//POST de 1 Usuario
+
+//PUT de 1 Usuario
+
+//! NO HACER EL DELETE (Los usuarios no tienen porque eliminarse)
---
+++ src/Middleware/userAuth.js
@@ -0,0 +1,3 @@
+//TODO Definir Middleware de autenticacion
+
+//* NOTA: Esto es un poquito complicado, si quieren esperen a mi ayuda ~ Xanth
