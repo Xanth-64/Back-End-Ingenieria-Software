084d652513b0f10b25cf19d20afb601cc7506125
dasimo <dsimoes@correo.unimet.edu.ve>
2021-06-29 23:38:46 -0400

    query sprint 6

--- src/Controllers/pedidoController.js
+++ src/Controllers/pedidoController.js
@@ -54,3 +54,155 @@ group by CAST(fecha AS DATE)`,
     res.status(500).json({ message: "ERROR", data: {} });
   }
 }
+
+// MONTO TOTAL SEMANAL
+export async function total_monto_semana(req, res) {
+  
+
+  try {
+    const pedido = await sequelize.models.pedido.findAll(req.body);
+
+    if (pedido) {
+      const pedido_semana = await sequelize.query(
+        `select sum(monto_tienda)from pedido 
+            where "createdAt" BETWEEN (select now() - interval '7days') AND (select now()) `,
+
+       
+      );
+
+      if (pedido_semana) {
+        return res.json({
+          message: `El monto total de la semana es`,
+          data: pedido_semana[0],
+        });
+      }
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({ message: "ERROR", data: {} });
+  }
+}
+
+// MONTO TOTAL MENSUAL
+export async function total_monto_mensual(req, res) {
+  
+
+  try {
+    const pedido = await sequelize.models.pedido.findAll(req.body);
+
+    if (pedido) {
+      const pedido_mensual = await sequelize.query(
+        `select sum(monto_tienda)from pedido 
+              where "createdAt" BETWEEN (select now() - interval '1month') AND (select now()) `,
+
+       
+      );
+
+      if (pedido_mensual) {
+        return res.json({
+          message: `El monto total de la semana es`,
+          data: pedido_mensual[0],
+        });
+      }
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({ message: "ERROR", data: {} });
+  }
+}
+
+// monto total anual
+export async function total_monto_anual(req, res) {
+  
+
+  try {
+    const pedido = await sequelize.models.pedido.findAll(req.body);
+
+    if (pedido) {
+      const pedido_anual = await sequelize.query(
+        `select sum(monto_tienda)from pedido 
+              where "createdAt" BETWEEN (select now() - interval '1year') AND (select now()) `,
+
+       
+      );
+
+      if (pedido_anual) {
+        return res.json({
+          message: `El monto total de la semana es`,
+          data: pedido_anual[0],
+        });
+      }
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({ message: "ERROR", data: {} });
+  }
+}
+
+// TRAE EL MONTO TOTAL AL ESPECEFICAR LA CANTIDAD DE MESES HACIA ATRAS
+export async function total_mensual_especifico(req, res) {
+  const meses= req.params.meses;
+ 
+  try {
+    const pedido = await sequelize.models.pedido.findAll(req.body);
+
+    if (pedido) {
+      const pedido_mensual = await sequelize.query(
+        `select sum(monto_tienda)from pedido 
+              where "createdAt" BETWEEN (select now() - interval ':mes month') AND (select now()) `,
+
+       {
+        type: QueryTypes.SELECT,
+        replacements: { mes: parseInt(meses)},
+      }
+    
+      );
+
+      if (pedido_mensual) {
+        return res.json({
+          message: `El monto total de la semana es`,
+          data: pedido_mensual [0],
+        });
+      }
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({ message: "ERROR", data: {} });
+  }
+}
+
+//topo 5 de los usuarios que han ralizado más compras a un negocio
+export async function topocinto_usuarios_compranmas(req, res) {
+  const id= req.params.id;
+ 
+  try {
+    const empren = await sequelize.models.pedido.findAll(req.body);
+
+    if (empren) {
+      const usuarios_empren = await sequelize.query(
+        `select count( usuario.id_usuario ) as cantidad, usuario.* from pedido
+                inner join  usuario ON "pedido"."usuarioIdUsuario"=usuario.id_usuario
+                inner join producto_pedidos ON "producto_pedidos"."pedidoIdPedido"=pedido.id_pedido
+                inner join producto ON producto.id_producto="producto_pedidos"."productoIdProducto"
+                where "emprendimientoIdNegocio"=(:id_empren)
+                group by( usuario.id_usuario )  order by cantidad desc limit 5 `,
+
+       {
+        type: QueryTypes.SELECT,
+        replacements: { id_empren: id},
+      }
+    
+      );
+
+      if (usuarios_empren) {
+        return res.json({
+          message: `El monto total de la semana es`,
+          data:  usuarios_empren ,
+        });
+      }
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({ message: "ERROR", data: {} });
+  }
+}
--- src/routes/pedido.js
+++ src/routes/pedido.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-import { pedido_dia_promedio } from "../Controllers/pedidoController";
+import { pedido_dia_promedio,total_monto_semana, total_mensual_especifico,total_monto_mensual, total_monto_anual,topocinto_usuarios_compranmas  } from "../Controllers/pedidoController";
 const defaultCrudCallbacks = require("../Controllers/pedidoController");
 import { linkProducts } from "../Controllers/pedidoController";
 //RUTA POST
@@ -20,4 +20,16 @@ router.get("/pedido_dia", pedido_dia_promedio);
 
 router.post("/linkProds/:id", linkProducts);
 
+// pedido del monto total de 1 semana
+router.get("/semana", total_monto_semana);
+// total de ganancias realizadas en 1 mes
+router.get("/mensual", total_monto_mensual);
+
+// total de ganancias realizadas en 1 año
+router.get("/anual", total_monto_anual);
+
+//total de las ganancias realizadas en una cantidad de meses especificada
+router.get("/mes/:meses", total_mensual_especifico);
+//topo 5 de los clientes que mas compran a un negocio
+router.get("/emprendi/:id", topocinto_usuarios_compranmas );
 module.exports = router;
