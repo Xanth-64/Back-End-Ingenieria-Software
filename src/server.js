8af3a84233bbe14237ff899db2ab7e66071d46fc
Andres Eloy <30258079@correo.unimet.edu.ve>
2021-07-03 17:40:16 -0400

    Cron Function Ready

--- src/server.js
+++ src/server.js
@@ -73,7 +73,8 @@ export const start = async () => {
     .catch((err) => {
       console.error(`Error al Conectar con la BD: ${err}`);
     });
-  cron.schedule("* * * * *", async () => {
+  //Hacer invisibles los productos de la gente que no ha pagado.
+  cron.schedule("0 5 * * *", async () => {
     try {
       const nonPremiumAccounts = await sequelize.query(
         'SELECT MAX(fecha_fin) AS fecha, "emprendimientoIdNegocio" AS id FROM "public"."suscripcion" GROUP BY "emprendimientoIdNegocio" HAVING MAX(fecha_fin) >= (:today)',
